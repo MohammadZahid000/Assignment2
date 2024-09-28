faae931807344ecb52d5e994ad0f908649a3d382
MohammadZahid000 <141526017+MohammadZahid000@users.noreply.github.com>
2024-09-29 03:33:21 +0530

    Add files via upload

---
+++ Assignment2.java
@@ -0,0 +1,31 @@
+package module2;
+
+public class Assignment2 {
+
+
+	    public static void main(String[] args) {
+	        int rows = 4; 
+
+	        
+	        for (int i = 1; i <= rows; i++) {
+	            
+	            for (int j = 1; j <= 4; j++) {
+	                System.out.print(j + " ");
+	            }
+
+	            
+	            System.out.print("      "); 
+
+	            
+	            for (int k = 0; k < 5; k++) {
+	                System.out.print(i + " ");
+	            }
+
+	        
+	            System.out.println();
+	        }
+	    }
+
+
+	}
+
