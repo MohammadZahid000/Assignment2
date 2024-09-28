2ea74e60b78a578486d923891aba79ccf0c20d7b
MohammadZahid000 <141526017+MohammadZahid000@users.noreply.github.com>
2024-09-28 19:27:43 +0530

    Update Assignment1.java

--- Assignment1.java
+++ Assignment1.java
@@ -4,11 +4,14 @@ public class Assignment1 {
 
 	public static void main(String[] args) {
 		for (int i = 0; i < 3; i++) {
-            
-            for (int j = 0; j < 5; j++) {
-                System.out.print("* "); 
+           
+            System.out.print("*   *");
+        
+            for (int j = 0; j < 3; j++) {
+                System.out.print(" *");
             }
-            System.out.println(); 
-        }
+         
+            System.out.println();
     }
+}
 }
